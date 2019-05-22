09f676f127b6ae7c50fbaf59a10974ec2bfe020a
CMohamed <chadad.mohamed@gmail.com>
2019-05-22 04:00:41 +0100

    fix layers visibility

--- src/app/carte/carte.component.ts
+++ src/app/carte/carte.component.ts
@@ -6,6 +6,7 @@ import OlTile from 'ol/source/Tile';
 import OlTileWMS from 'ol/source/TileWMS';
 import OlTileLayer from 'ol/layer/Tile';
 import OlView from 'ol/View';
+import {Group as LayerGroup, Tile as TileLayer} from 'ol/layer.js';
 
 import { fromLonLat } from 'ol/proj';
 
@@ -30,10 +31,12 @@ export class CarteComponent implements OnInit, AfterViewInit {
     });
 
     this.couches[0] = new OlTileLayer({
+      title: 'BaseMap',
       source: this.source
     });
 
     this.couches[1] = new OlTileLayer({
+      title: 'Regions',
       source: new OlTileWMS({
         url: 'http://localhost:8080/geoserver/EHTP/wms?',
         params: {layers: 'EHTP:regions', tiled: true, transparence: true}
@@ -50,12 +53,32 @@ export class CarteComponent implements OnInit, AfterViewInit {
     this.map = new OlMap({
       target: 'map',
       layers: this.couches,
+      // new LayerGroup({
+      //   layers: this.couches
+      // }),
       view: this.view
     });
   }
 
   constructor() { }
 
+  togle(j) {
+    console.log(this.map.getLayers());
+    this.map.getLayers().forEach(function (layer, i) {
+      if (i === j) {
+        const test = layer.getVisible();
+        layer.setVisible(!test);
+      }
+     });
+    // this.couches.splice(i, 1);
+    // console.log(this.couches);
+    // this.map = null;
+    // this.map = new OlMap({
+    //   target: 'map',
+    //   layers: this.couches,
+    //   view: this.view
+    // });
+  }
 
   ngAfterViewInit() {
     this.map.setTarget('map');
